f6ce78679a18b198bbfad7867a556fc9a3b00339
DESKTOP-J8D0VU7\Hasan <seifi.rsa@gmail.com>
2017-10-11 16:03:24 +0330

    The first commit- works just for Nrf51x families

---
+++ Hex Analyzer/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Hex_Analyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileLoader = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label_compiled = new System.Windows.Forms.Label();
+            this.txt_compiled_file = new System.Windows.Forms.TextBox();
+            this.btn_compiled = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label_flash = new System.Windows.Forms.Label();
+            this.text_flash = new System.Windows.Forms.TextBox();
+            this.txt_flash_file = new System.Windows.Forms.TextBox();
+            this.btn_load_flash = new System.Windows.Forms.Button();
+            this.program_section = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.text_compiled = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.text_compiled);
+            this.groupBox1.Controls.Add(this.label_compiled);
+            this.groupBox1.Controls.Add(this.txt_compiled_file);
+            this.groupBox1.Controls.Add(this.btn_compiled);
+            this.groupBox1.Location = new System.Drawing.Point(307, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(269, 240);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Compiled Hex";
+            // 
+            // label_compiled
+            // 
+            this.label_compiled.AutoSize = true;
+            this.label_compiled.Location = new System.Drawing.Point(6, 194);
+            this.label_compiled.Name = "label_compiled";
+            this.label_compiled.Size = new System.Drawing.Size(43, 13);
+            this.label_compiled.TabIndex = 9;
+            this.label_compiled.Text = "------------";
+            // 
+            // txt_compiled_file
+            // 
+            this.txt_compiled_file.Location = new System.Drawing.Point(6, 33);
+            this.txt_compiled_file.Name = "txt_compiled_file";
+            this.txt_compiled_file.Size = new System.Drawing.Size(212, 20);
+            this.txt_compiled_file.TabIndex = 5;
+            // 
+            // btn_compiled
+            // 
+            this.btn_compiled.Location = new System.Drawing.Point(224, 33);
+            this.btn_compiled.Name = "btn_compiled";
+            this.btn_compiled.Size = new System.Drawing.Size(38, 20);
+            this.btn_compiled.TabIndex = 4;
+            this.btn_compiled.Text = "...";
+            this.btn_compiled.UseVisualStyleBackColor = true;
+            this.btn_compiled.Click += new System.EventHandler(this.btn_compiled_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label_flash);
+            this.groupBox2.Controls.Add(this.text_flash);
+            this.groupBox2.Controls.Add(this.txt_flash_file);
+            this.groupBox2.Controls.Add(this.btn_load_flash);
+            this.groupBox2.Location = new System.Drawing.Point(12, 57);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(271, 240);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Read-back Hex";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // label_flash
+            // 
+            this.label_flash.AutoSize = true;
+            this.label_flash.Location = new System.Drawing.Point(6, 194);
+            this.label_flash.Name = "label_flash";
+            this.label_flash.Size = new System.Drawing.Size(43, 13);
+            this.label_flash.TabIndex = 10;
+            this.label_flash.Text = "------------";
+            // 
+            // text_flash
+            // 
+            this.text_flash.Location = new System.Drawing.Point(6, 59);
+            this.text_flash.Multiline = true;
+            this.text_flash.Name = "text_flash";
+            this.text_flash.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.text_flash.Size = new System.Drawing.Size(256, 132);
+            this.text_flash.TabIndex = 7;
+            this.text_flash.WordWrap = false;
+            // 
+            // txt_flash_file
+            // 
+            this.txt_flash_file.Location = new System.Drawing.Point(6, 33);
+            this.txt_flash_file.Name = "txt_flash_file";
+            this.txt_flash_file.Size = new System.Drawing.Size(212, 20);
+            this.txt_flash_file.TabIndex = 3;
+            // 
+            // btn_load_flash
+            // 
+            this.btn_load_flash.Location = new System.Drawing.Point(224, 33);
+            this.btn_load_flash.Name = "btn_load_flash";
+            this.btn_load_flash.Size = new System.Drawing.Size(38, 20);
+            this.btn_load_flash.TabIndex = 2;
+            this.btn_load_flash.Text = "...";
+            this.btn_load_flash.UseVisualStyleBackColor = true;
+            this.btn_load_flash.Click += new System.EventHandler(this.btn_load_flash_Click);
+            // 
+            // program_section
+            // 
+            this.program_section.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.program_section.FormattingEnabled = true;
+            this.program_section.Items.AddRange(new object[] {
+            "Code",
+            "Configuration"});
+            this.program_section.Location = new System.Drawing.Point(109, 12);
+            this.program_section.Name = "program_section";
+            this.program_section.Size = new System.Drawing.Size(189, 21);
+            this.program_section.TabIndex = 4;
+            this.program_section.SelectedIndexChanged += new System.EventHandler(this.program_section_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Program Section :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Varify";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // text_compiled
+            // 
+            this.text_compiled.Location = new System.Drawing.Point(6, 59);
+            this.text_compiled.Multiline = true;
+            this.text_compiled.Name = "text_compiled";
+            this.text_compiled.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.text_compiled.Size = new System.Drawing.Size(256, 132);
+            this.text_compiled.TabIndex = 10;
+            this.text_compiled.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 334);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.program_section);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hex Verificator";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog FileLoader;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_flash_file;
+        private System.Windows.Forms.Button btn_load_flash;
+        private System.Windows.Forms.TextBox txt_compiled_file;
+        private System.Windows.Forms.Button btn_compiled;
+        private System.Windows.Forms.TextBox text_flash;
+        private System.Windows.Forms.ComboBox program_section;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_compiled;
+        private System.Windows.Forms.Label label_flash;
+        private System.Windows.Forms.TextBox text_compiled;
+    }
+}
+
---
+++ Hex Analyzer/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hex_Analyzer
+{
+    public partial class Form1 : Form
+    {
+        String FlashCodeSection;
+        String FlashConfigSection;
+        String CompiledCodeSection;
+        String CompiledConfigSection;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_load_flash_Click(object sender, EventArgs e)
+        {
+            Stream hexFileFlash;
+            String hexFileFlashStr;
+            int i = 0;
+            int totalSize=0, totalConfigSize = 0;
+            byte[] hexFileFlashBytes;
+            FileLoader.Filter = "Intel Hex File|*.hex";
+            FileLoader.Title = "Load Flash Hex File";
+            FileLoader.ShowDialog();
+            txt_flash_file.Text = FileLoader.FileName;
+            hexFileFlash = FileLoader.OpenFile();
+            hexFileFlashBytes = new byte[hexFileFlash.Length];
+            hexFileFlash.Read(hexFileFlashBytes, 0, (int)hexFileFlash.Length);
+            hexFileFlash.Close();
+
+            hexFileFlashStr = Encoding.ASCII.GetString(hexFileFlashBytes);
+
+            i = 0;
+            String[] Records = hexFileFlashStr.Split(':');
+            int upperAddress = 0, nextRecAddress = -1;
+            bool IsCodeZone = true;
+            FlashCodeSection = "";
+            FlashConfigSection = "";
+            // Record Parser
+            while (i < Records.Length)
+            {
+                String currentRecord = Records[i++];
+                char[] enter = new char[] { '\r', '\n' };
+                currentRecord = currentRecord.Trim(enter);
+                if (currentRecord == "")
+                    continue;
+                byte recordLen = Enumerable.Range(0, 2).Where(x => x % 2 == 0).Select(x => Convert.ToByte(currentRecord.Substring(x, 2), 16)).ToArray()[0];
+                byte[] recordAddrByte = Enumerable.Range(2, 4).Where(x => x % 2 == 0).Select(x => Convert.ToByte(currentRecord.Substring(x, 2), 16)).ToArray();
+                int recordAddr = recordAddrByte[0] * 256 + recordAddrByte[1];
+                byte recordType = Enumerable.Range(6, 2).Where(x => x % 2 == 0).Select(x => Convert.ToByte(currentRecord.Substring(x, 2), 16)).ToArray()[0];
+                byte[] recordData = Enumerable.Range(8, recordLen * 2).Where(x => x % 2 == 0).Select(x => Convert.ToByte(currentRecord.Substring(x, 2), 16)).ToArray();
+                byte recordCrc = Enumerable.Range(recordLen * 2 + 8, 2).Where(x => x % 2 == 0).Select(x => Convert.ToByte(currentRecord.Substring(x, 2), 16)).ToArray()[0];
+                String DataHex = ByteArrayToString(recordData);
+
+                if (recordType == 4)
+                {
+                    upperAddress = recordData[0] * 256 + recordData[1];
+                }
+                else if (recordType == 0)
+                {
+                    int completeAddress = ((upperAddress << 16) | (recordAddr & 0xFFFF));
+
+                    // Make sure data are aligned
+                    if (recordData.Length % 4 != 0)
+                        DataHex += new String('F', (4 - (recordData.Length % 4)) * 2);
+
+
+                    if (nextRecAddress == -1)
+                        nextRecAddress = completeAddress;
+
+                    if (nextRecAddress != completeAddress)
+                    {
+                        IsCodeZone = false;
+                    }
+                    else if(IsCodeZone)
+                        FlashCodeSection += DataHex + "\r\n";
+                    else if (!IsCodeZone)
+                        FlashConfigSection += DataHex + "\r\n";
+
+                    if (IsCodeZone)
+                        totalSize += (DataHex.Length / 2);
+                    else if (!IsCodeZone)
+                        totalConfigSize += (DataHex.Length / 2);
+
+                    nextRecAddress = completeAddress + recordLen;
+                }
+                else if (recordType == 1)
+                {
+                    break;
+                }
+            }
+            if(program_section.SelectedIndex == 0)
+                text_flash.Text = FlashCodeSection;
+            else
+                text_flash.Text = FlashConfigSection;
+
+            label_flash.Text = String.Format("Code Size : {0}\nConfiguration Size : {1}", totalSize, totalConfigSize);
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            program_section.SelectedIndex = 0;
+        }
+
+        private void program_section_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (program_section.SelectedIndex == 0)
+            {
+                text_flash.Text = FlashCodeSection;
+                text_compiled.Text = CompiledCodeSection;
+            }
+            else
+            {
+                text_flash.Text = FlashConfigSection;
+                text_compiled.Text = CompiledConfigSection;
+            }
+        }
+        public static string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:X2}", b);
+            return hex.ToString();
+        }
+        private void btn_compiled_Click(object sender, EventArgs e)
+        {
+            Stream hexFileCompiled;
+            String hexFileCompiledStr;
+            int i = 0;
+            int totalSize = 0, totalConfigSize = 0;
+            byte[] hexFileCompiledBytes;
+            FileLoader.Filter = "Intel Hex File|*.hex";
+            FileLoader.Title = "Load Compiled Hex File";
+            FileLoader.ShowDialog();
+            txt_compiled_file.Text = FileLoader.FileName;
+            hexFileCompiled = FileLoader.OpenFile();
+            hexFileCompiledBytes = new byte[hexFileCompiled.Length];
+            hexFileCompiled.Read(hexFileCompiledBytes, 0, (int)hexFileCompiled.Length);
+            hexFileCompiled.Close();
+
+            hexFileCompiledStr = Encoding.ASCII.GetString(hexFileCompiledBytes);
+
+            i = 0;
+            String[] Records = hexFileCompiledStr.Split(':');
+            int upperAddress = 0, nextRecAddress = -1;
+            bool IsCodeZone = true;
+            CompiledCodeSection = "";
+            CompiledConfigSection = "";
+            // Record Parser
+            while (i < Records.Length)
+            {
+                String currentRecord = Records[i++];
+                char[] enter = new char[] { '\r', '\n' };
+                currentRecord = currentRecord.Trim(enter);
+                if (currentRecord == "")
+                    continue;
+                byte recordLen = Enumerable.Range(0, 2).Where(x => x % 2 == 0).Select(x => Convert.ToByte(currentRecord.Substring(x, 2), 16)).ToArray()[0];
+                byte[] recordAddrByte = Enumerable.Range(2, 4).Where(x => x % 2 == 0).Select(x => Convert.ToByte(currentRecord.Substring(x, 2), 16)).ToArray();
+                int recordAddr = recordAddrByte[0] * 256 + recordAddrByte[1];
+                byte recordType = Enumerable.Range(6, 2).Where(x => x % 2 == 0).Select(x => Convert.ToByte(currentRecord.Substring(x, 2), 16)).ToArray()[0];
+                byte[] recordData = Enumerable.Range(8, recordLen * 2).Where(x => x % 2 == 0).Select(x => Convert.ToByte(currentRecord.Substring(x, 2), 16)).ToArray();
+                byte recordCrc = Enumerable.Range(recordLen * 2 + 8, 2).Where(x => x % 2 == 0).Select(x => Convert.ToByte(currentRecord.Substring(x, 2), 16)).ToArray()[0];
+                String DataHex = ByteArrayToString(recordData);
+                if (recordType == 4)
+                {
+                    upperAddress = recordData[0] * 256 + recordData[1];
+                }
+                else if (recordType == 0)
+                {
+
+                    int completeAddress = ((upperAddress << 16) | (recordAddr & 0xFFFF));
+
+                    // Make sure data are aligned
+                    if (recordData.Length % 4 != 0)
+                        DataHex += new String('F', (4 - (recordData.Length % 4)) * 2);
+
+                    if (nextRecAddress == -1)
+                        nextRecAddress = completeAddress;
+
+                    if (nextRecAddress != completeAddress)
+                    {
+                        IsCodeZone = false;
+                    }
+                    else if (IsCodeZone)
+                        CompiledCodeSection += DataHex + "\r\n";
+                    else if (!IsCodeZone)
+                        CompiledConfigSection += DataHex + "\r\n";
+
+                    if (IsCodeZone)
+                        totalSize += (DataHex.Length/2);
+                    else if (!IsCodeZone)
+                        totalConfigSize += (DataHex.Length/2);
+
+                    nextRecAddress = completeAddress + recordLen;
+                }
+                else if (recordType == 1)
+                {
+                    break;
+                }
+
+                // Set hex map
+
+
+            }
+            if (program_section.SelectedIndex == 0)
+                text_compiled.Text = CompiledCodeSection;
+            else
+                text_compiled.Text = CompiledConfigSection;
+
+            label_compiled.Text = String.Format("Code Size : {0}\nConfiguration Size : {1}", totalSize, totalConfigSize);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (program_section.SelectedIndex == 0)
+            {
+                if (FlashCodeSection == CompiledCodeSection)
+                    MessageBox.Show(this, "Verified Successfull!", "Verifivation",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                else
+                    MessageBox.Show(this, "Verification Failed!", "Verifivation", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            else
+            {
+                if (FlashConfigSection == CompiledConfigSection)
+                    MessageBox.Show(this, "Verified Successfull!", "Verifivation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show(this, "Verification Failed!", "Verifivation", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+        }
+    }
+}
